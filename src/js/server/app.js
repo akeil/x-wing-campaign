d2662b8f4ec355adbb147d153afb42621eefc62d
akeil <akeil@akeil.net>
2016-05-30 22:52:31 +0200

    Fix `main` entry in package.json

--- src/js/server/app.js
+++ src/js/server/app.js
@@ -12,8 +12,8 @@ var express = require('express'),
 
 // for openshift or local installation
 var port, host, dbname, dburl;
-port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var dbHost, dbPort, dbUser, dbPass, dbName;
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
